781e214e308a1e89507a5c64b48f83ebea0a709a
Leonardo Delgado <sdelgadom@unal.edu.co>
2020-11-17 00:49:22 -0500

    genera rutas para más cercanos con recursos no muy bien ubicados

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -43,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Policia> policias = new ArrayList<Policia>();;
     private List<Policia> elegibles = new ArrayList<Policia>();;
+
+    private int numRutasCalculadas = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "HERE Rendering Engine attached.");
             }
         });
-
+        createPolicias();
 //        defaultMarker.setCoordinate(new GeoCoordinate);
 //        mapView.getMapScene().addMapMarker(defaultMarker);
     }
@@ -93,8 +95,12 @@ public class MainActivity extends AppCompatActivity {
                         if (routingError == null) {
                                  elegibles.get(i).setDistT(routes.get(0).getDurationInSeconds());
                                  elegibles.get(i).setRoute(routes.get(0));
-                                 showRouteOnMap(elegibles.get(i).Route);
-
+//                                 showRouteOnMap(elegibles.get(i).Route, 20);
+                                 numRutasCalculadas++;
+                                 if(numRutasCalculadas == 10){
+                                     showRutas();
+                                     numRutasCalculadas = 0;
+                                 }
                         } else {
                             new AlertDialog.Builder(MainActivity.this).setTitle("Error while calculating a route:").setMessage(routingError.toString()).show();
                         }
@@ -106,47 +112,82 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void matcher(double lat, double lng) {
         Collections.sort(policias);
         System.out.println(policias.size()+"AASDDA");
-        int i = 0;
-        for (Policia p : policias) {
-            elegibles.add(p);
-            i++;
-            if (i == 10) break;
+        for (int i = 0; i < 10; ++i) {
+            elegibles.add(policias.get(i));
         }
         for (int j = 0; j < elegibles.size(); j++) {
             routeMap (new GeoCoordinates(lat,lng), j);
         }
 
     }
-    public void createPolicias(double lat, double lng){
+
+    public void addMarker(int img, GeoCoordinates coordenadas){
+        MapImage mapImage = MapImageFactory.fromBitmap(resizeImage(getApplicationContext(), img, 50, 80));
+        MapMarker mapMarker = new MapMarker(coordenadas, mapImage);
+        mapMarker.setImage(mapImage);
+        mapView.getMapScene().addMapMarker(mapMarker);
+    }
+    public double calcEuclideanDist(GeoCoordinates pos1, GeoCoordinates pos2){
+        return Math.sqrt( (pos1.latitude - pos2.latitude)*(pos1.latitude - pos2.latitude)  + (pos1.longitude - pos2.longitude)*(pos1.longitude - pos2.longitude));
+    }
+
+    public void createPolicia(GeoCoordinates coordenadas){
+        Policia p = new Policia(coordenadas);
+        policias.add(p);
+        addMarker(R.drawable.police, coordenadas);
+    }
+    public void createPolicias(){
 
 //        5.509604, -73.373164 CAI 1
 
+
+        //TUNJA
         double i = 5.5016;
         double j = -73.375;
-        while(i < 5.5865){
+        GeoCoordinates coordenadas = new GeoCoordinates(i, j);
+        Policia p = new Policia(coordenadas);
+        while(i < 5.5565){
 
             j += 0.001;
-            i += 0.001;
-
-            GeoCoordinates coordenadas = new GeoCoordinates(i, j);
-//            MapImage mapImage = MapImageFactory.fromResource(getApplicationContext().getResources(), R.drawable.police);
-            MapImage mapImage = MapImageFactory.fromBitmap(resizeImage(getApplicationContext(), R.drawable.police, 50, 80));
-//            Anchor2D anchor2D = new Anchor2D(0.5F, .5F);
-            MapMarker mapMarker = new MapMarker(coordenadas, mapImage);
-            mapMarker.setImage(mapImage);
-            mapView.getMapScene().addMapMarker(mapMarker);
-            Policia p = new Policia(coordenadas);
-            double lat2 = p.coor.latitude;
-            double lng2 = p.coor.longitude;
-            p.dist = Math.sqrt( (lat2 - lat)*(lat2 - lat)  + (lng2 - lng)*(lng2 - lng) );
-            policias.add(p);
+            i += 0.002;
+            coordenadas = new GeoCoordinates(i, j);
+            createPolicia(coordenadas);
         }
 
+        //SAMACA
+        coordenadas = new GeoCoordinates(5.493094, -73.487034);
+        createPolicia(coordenadas);
+        coordenadas = new GeoCoordinates(5.491094, -73.487034);
+        createPolicia(coordenadas);
 
+
+        //SOTAQUIRA
+        coordenadas = new GeoCoordinates(5.764929, -73.246165);
+        createPolicia(coordenadas);
+        coordenadas = new GeoCoordinates(5.764929, -73.248165);
+        createPolicia(coordenadas);
+
+        //MOTAVITA
+        coordenadas = new GeoCoordinates(5.576905, -73.369377);
+        createPolicia(coordenadas);
+        coordenadas = new GeoCoordinates(5.576905, -73.367377);
+        createPolicia(coordenadas);
+    }
+
+    public void recalcPolicias(double lat, double lng){
+        GeoCoordinates destino = new GeoCoordinates(lat, lng);
+        numRutasCalculadas = 0;
+        for(Policia p : policias){
+            if(p.mapPolyline != null) mapView.getMapScene().removeMapPolyline(p.mapPolyline);
+            p.dist = calcEuclideanDist(destino, p.coor);
+            p.distT = 0;
+            p.mapPolyline = null;
+            p.setRoute(null);
+        }
+        elegibles.clear();
     }
     private void showRouteDetails(Route route) {
         long estimatedTravelTimeInSeconds = route.getDurationInSeconds();
@@ -173,27 +214,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createRandomCoordinatesTunja(View v){
-        double lat = 5.5016;
-        double lng = -73.375;
-        createPolicias(lat, lng);
-        matcher(lat,lng);
+        double lat = 5.5316;
+        double lng = -73.365;
         loadMapScene(lat, lng);
     }
     public void createRandomCoordinatesMotavita(View v){
         double lat = 5.578000;
         double lng = -73.368144;
-
         loadMapScene(lat, lng);
     }
     public void createRandomCoordinatesSomaca(View v){
 
         double lat = 5.492345;
         double lng = -73.486189;
-
         loadMapScene(lat, lng);
     }
     private void loadMapScene(double lat, double lng) {
         // Load a scene from the HERE SDK to render the map with a map scheme.
+        mapView = findViewById(R.id.map_view);
         mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, new MapScene.LoadSceneCallback() {
             @Override
             public void onLoadScene(@Nullable MapError mapError) {
@@ -213,7 +251,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
+//        mapView.getMapScene().
+        recalcPolicias(lat, lng);
+        matcher(lat,lng);
     }
 
     @Override
@@ -267,8 +307,20 @@ public class MainActivity extends AppCompatActivity {
         return resizedBitmap;
 
     }
-    float widthInPixels = 100;
-    private void showRouteOnMap(Route route) {
+
+    private void showRutas(){
+        Collections.sort(elegibles);
+
+        for(int i = elegibles.size() - 1; i > 0 ; --i){
+
+            if(elegibles.get(i).Route != null) showRouteOnMap(elegibles.get(i).Route, i, elegibles.get(i));
+        }
+        if(elegibles.get(0).Route != null) showRouteOnMap(elegibles.get(0).Route, -1, elegibles.get(0));
+    }
+
+
+    float widthInPixels = 10;
+    private void showRouteOnMap(Route route, int puesto, Policia p) {
         // Show route as polyline.
         GeoPolyline routeGeoPolyline;
         try {
@@ -277,13 +329,14 @@ public class MainActivity extends AppCompatActivity {
             // It should never happen that a route polyline contains less than two vertices.
             return;
         }
-
-        widthInPixels = widthInPixels -10;
+        if(puesto == -1) widthInPixels = 20;
+        widthInPixels = widthInPixels;
         MapPolyline routeMapPolyline = new MapPolyline(routeGeoPolyline,
                 widthInPixels,
-                Color.valueOf(widthInPixels, 0.56f, 0.54f, 0.63f)); // RGBA
-
+                Color.valueOf((puesto*20)/255f, (255-puesto*10)/255f, (puesto*20)/255f, puesto*10/255f)); // RGBA
+        p.mapPolyline = routeMapPolyline;
         mapView.getMapScene().addMapPolyline(routeMapPolyline);
+//        mapView.getMapScene().
 
     }
 }
--- app/src/main/java/com/example/helloapp/Policia.java
+++ app/src/main/java/com/example/helloapp/Policia.java
@@ -1,6 +1,7 @@
 package com.example.helloapp;
 
 import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.mapview.MapPolyline;
 import com.here.sdk.routing.*;
 public class Policia implements Comparable{
     public GeoCoordinates coor;
@@ -8,18 +9,27 @@ public class Policia implements Comparable{
     public double distT = 0;
     public boolean t = true;
     public Route Route = null;
+    public MapPolyline mapPolyline = null;
+
 
     Policia(GeoCoordinates coor){
         this.coor = coor;
+        distT = 0;
     }
 
 
     @Override
     public int compareTo(Object o) {
         Policia p1 = (Policia) o;
+        if(p1.distT == 0 && this.distT == 0){
+            if(p1.dist > this.dist) return -1;
+            else if(p1.dist < this.dist) return 1;
+            return 0;
+        }
 
-        return (int) (p1.dist - this.dist);
-
+        if(p1.distT > this.distT) return -1;
+        else if(p1.distT > this.distT) return 1;
+        else return 0;
     }
 
     public void setRoute(com.here.sdk.routing.Route route) {
